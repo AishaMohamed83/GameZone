a7d26b3eac71597262d1455f2b923164be4cd708
AishaMohamed83 <89880465+AishaMohamed83@users.noreply.github.com>
2024-11-29 12:46:15 +0200

    Add files via upload

---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using GameZone.Data;
+global using GameZone.Models;
+global using GameZone.ViewModels;
+global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+
+//@addTagHelper *, Microsoft.AspNetCore.Mvc.TagHelpers
